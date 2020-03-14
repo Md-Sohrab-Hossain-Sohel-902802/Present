1711785f1068d385946f16d67d1f0a4d4f2500e5
Sohrab <mdsohrabhossainsohel902833@gmail.com>
2020-03-13 20:58:28 -0700

    New 2 Method Added

--- Hellow.java
+++ Hellow.java
@@ -2,12 +2,17 @@ public class Hellow{
     public static void main(String[] args){
         System.out.println("Vs code is Osam");
         Gretings("Md Sohrab Hossain Sohel");
+        Sohel();
     }
 
 
 
     public static void  Gretings(String gretings){
         System.out.println(gretings);
+    }
+    public static void Sohel(){
+
+        System.out.println("Md Sohrab Hossain Sohel");
     }
 
 }
