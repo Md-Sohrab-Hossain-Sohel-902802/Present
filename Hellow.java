f4767758aa4951393ae913d08591bf52ac210b94
Sohrab <mdsohrabhossainsohel902833@gmail.com>
2020-03-13 20:55:34 -0700

    New Method call display to hellow.java

--- Hellow.java
+++ Hellow.java
@@ -1,5 +1,13 @@
 public class Hellow{
     public static void main(String[] args){
         System.out.println("Vs code is Osam");
+        Gretings("Md Sohrab Hossain Sohel");
     }
+
+
+
+    public static void  Gretings(String gretings){
+        System.out.println(gretings);
+    }
+
 }
